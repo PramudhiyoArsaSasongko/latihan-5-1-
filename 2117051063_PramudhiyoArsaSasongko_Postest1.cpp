9bfac76d90517417288d0da4013fcc1a060eec5a
PramudhiyoArsaSasongko <92429646+PramudhiyoArsaSasongko@users.noreply.github.com>
2021-12-04 01:14:37 +0700

    Add files via upload

---
+++ 2117051063_PramudhiyoArsaSasongko_Postest1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	/* Nama : Pramudhiyo Arsa Sasongko
+	   Npm  : 2117051063
+	   Kelas: B, ILKOM21*/
+
+	//string
+	string npm;
+	string tanggal;
+	string bulan;
+	string tahun;
+	
+	//input
+	cin>>npm;
+	cin>>tanggal;
+	cin>>bulan;
+	cin>>tahun;
+	
+	//output
+	cout<<endl;
+	cout<<"Username : "<<npm<<endl;
+	cout<<"Password : "<<tahun<<bulan<<tanggal<<endl;
+	
+	return 0;
+}
